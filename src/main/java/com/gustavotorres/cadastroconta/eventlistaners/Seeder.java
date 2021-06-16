4b5897187937a8ce7bd879f8df7d20d0442b8be1
gustavotrr <gustavotrr@gmail.com>
2021-06-16 02:57:49 -0300

    Corrigindo o Seeder

--- src/main/java/com/gustavotorres/cadastroconta/eventlistaners/Seeder.java
+++ src/main/java/com/gustavotorres/cadastroconta/eventlistaners/Seeder.java
@@ -3,6 +3,8 @@ package com.gustavotorres.cadastroconta.eventlistaners;
 import java.math.BigInteger;
 import java.util.Arrays;
 
+import javax.transaction.Transactional;
+
 import com.gustavotorres.cadastroconta.entities.ParametroFaixaScoreELimite;
 import com.gustavotorres.cadastroconta.repositories.ParametroFaixaScoreELimiteRepository;
 
@@ -22,22 +24,27 @@ public class Seeder {
         seedParametroFaixaScoreELimiteTable();
     }
 
+    @Transactional
     private void seedParametroFaixaScoreELimiteTable() {
-        System.out.println(" >> Iniciando o seed da tabela parametros_faixa_score_e_limites");
 
-        parametroFaixaScoreELimiteRepository.saveAll(Arrays.asList(
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("0")).limiteCartaoEmCentavos(BigInteger.valueOf(0L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(0L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("1")).limiteCartaoEmCentavos(BigInteger.valueOf(0L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(0L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("2")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("3")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("4")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("5")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("6")).limiteCartaoEmCentavos(BigInteger.valueOf(200000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(200000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("7")).limiteCartaoEmCentavos(BigInteger.valueOf(200000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(200000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("8")).limiteCartaoEmCentavos(BigInteger.valueOf(200000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(200000L)).build(),
-            ParametroFaixaScoreELimite.builder().score(Short.valueOf("9")).limiteCartaoEmCentavos(BigInteger.valueOf(1500000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(500000L)).build()
-        ));
+        if (parametroFaixaScoreELimiteRepository.findAll().isEmpty()){
+            
+            System.out.println(" >> Iniciando o seed da tabela parametros_faixa_score_e_limites");
 
-        System.out.println(" >> Finalizado o seed da tabela parametros_faixa_score_e_limites");
+            parametroFaixaScoreELimiteRepository.saveAll(Arrays.asList(
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("0")).limiteCartaoEmCentavos(BigInteger.valueOf(0L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(0L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("1")).limiteCartaoEmCentavos(BigInteger.valueOf(0L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(0L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("2")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("3")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("4")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("5")).limiteCartaoEmCentavos(BigInteger.valueOf(20000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(100000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("6")).limiteCartaoEmCentavos(BigInteger.valueOf(200000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(200000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("7")).limiteCartaoEmCentavos(BigInteger.valueOf(200000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(200000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("8")).limiteCartaoEmCentavos(BigInteger.valueOf(200000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(200000L)).build(),
+                ParametroFaixaScoreELimite.builder().score(Short.valueOf("9")).limiteCartaoEmCentavos(BigInteger.valueOf(1500000L)).limiteChequeEspecialEmCentavos(BigInteger.valueOf(500000L)).build()
+            ));
+
+            System.out.println(" >> Finalizado o seed da tabela parametros_faixa_score_e_limites");
+        }
     }
 }
